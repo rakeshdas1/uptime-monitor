9bd52f64cb3715e10fde11a65cfe9794468b848a
Rakesh Das <rakeshdaskowshik@gmail.com>
2021-02-13 22:34:34 -0600

    Added testing code

--- main_test.go
+++ main_test.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"log"
 	"os"
 	"testing"
-	"log"
+
+	"github.com/joho/godotenv"
 )
 
 var a App
+
 func TestMain(m *testing.M) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file! %s", err)
+	}
 	a.Initialize(os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
 	ensureTableExists()
 	code := m.Run()
